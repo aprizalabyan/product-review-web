4c063906c04cb79efb6ab726ddbe73c96a9ff574
aprizalabyan <abi@jkt1.ebdesk.com>
2025-12-27 23:44:58 +0700

    Add api-proxy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/product-review-api": {
+        target: "https://fastapi-mongodb-production-34a8.up.railway.app/api/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/product-review-api/, ""),
+      },
+    },
+  },
+});
